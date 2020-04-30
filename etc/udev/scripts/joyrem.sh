7c53eccc8d2939102742dc0741d0e806c8f509a5
1edenec <55366594+1edenec@users.noreply.github.com>
2020-04-30 22:22:18 +0300

    xbox script added instead

--- etc/udev/scripts/joyrem.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-STAT=$1
-DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
-export DBUS_SESSION_BUS_ADDRESS
-xset +dpms
-xset s on
-su led -c "notify-send  'Sytem Message' 'Xbox Gamepad $STAT disconnected'"
-
