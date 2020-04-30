67ea0ddf54a2d078e3ef6445280ce21901789ccd
1edenec <55366594+1edenec@users.noreply.github.com>
2020-04-30 22:21:59 +0300

    xbox script added instead

--- etc/udev/scripts/joyadd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-STAT=$1
-DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
-export DBUS_SESSION_BUS_ADDRESS
-xset -dpms
-xset s off
-su led -c "notify-send  'System Message' 'Xbox Gamepad $STAT connected'"
